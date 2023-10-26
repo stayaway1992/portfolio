c9227f9ca92cc33fa5252e4c7388e163fcc0d1f3
Alix <frozzsmurf@gmail.com>
2023-10-26 18:44:49 +0200

    responsive

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //toggle icon navbar
 let menuIcon = document.querySelector("#menu-icon");
-let navbar = document.querySelector("navbar");
+let navbar = document.querySelector(".navbar");
 menuIcon.onclick = () => {
     menuIcon.classList.toggle("bx-x");
     navbar.classList.toggle("active");
@@ -31,4 +31,10 @@ window.onscroll = () => {
     let header = document.querySelector("header");
 
     header.classList.toggle("sticky", window.scrollY > 100);
+
+    //remove toggle icon and navbar when click nav links
+    menuIcon.classList.remove("bx-x");
+    navbar.classList.remove("active");
 }
+
+
